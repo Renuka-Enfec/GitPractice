5135771efe99f132a93ef3f85990afe69fce6426
Renu <renu012379k@gmail.com>
2025-10-24 12:34:24 +0530

    Commiting testerCode to BranchA

---
+++ src/praCtice/TesterCode.java
@@ -0,0 +1,13 @@
+package praCtice;
+
+public class TesterCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Additon of two number is: "+c);
+	}
+
+}
