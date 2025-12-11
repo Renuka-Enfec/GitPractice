be6fbfa3e4c0da8ca022e028af4a6d4bad9a47ec
Renuka-Enfec <renuka.kaparatti@enfec.com>
2025-12-11 10:39:36 +0530

    Subtraction of two number

---
+++ src/praCtice/SubstractionOfTwoNumbers.java
@@ -0,0 +1,12 @@
+public class SubstractionOfTwoNumbers {
+
+	public static void main(String[] args) {
+	
+		int m=15;
+		int n=5;
+		int o=m-n;
+		System.out.println("Substraction of two number is: "+o);
+
+	}
+
+}
